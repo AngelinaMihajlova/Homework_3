03b90693f5d4a32f639055d249307bcf823e17d8
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-07-06 14:31:13 +0300

    New folder Task_21

---
+++ Task_21/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+// AB = √ (Xb-Xa)2 + (Yb-Ya)2 + (Zb-Za)2
+
+double FindDistance (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double distance = 0;
+
+    distance = Math.Sqrt (Math.Pow (x2 - x1, 2) + Math.Pow (y2 - y1, 2) + Math.Pow (z2 - z1, 2));
+    return distance;
+}
+
+FindDistance (x1 = 3, y1 = 6, z1 = 8, x2 = 2, y2 = 1, x2 = -7);
