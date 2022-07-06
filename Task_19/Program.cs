867da493f6f05ac33bbb6ba39095fcdc6d9c2778
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-07-06 13:41:31 +0300

    New folder Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+bool Palindrome (int[] array)
+{
+    int N = array.Length;
+    bool result = false;
+    for (int i = 0; i < N / 2; i ++)
+    {
+        if (array[i] != array[N-i-1]) 
+        {
+            Console.WriteLine ("It's not a palindrome");
+            return result;
+        }
+    }
+    result = true;
+    
+    Console.WriteLine ("This is a palindrome");
+    return result;
+}
+
+
