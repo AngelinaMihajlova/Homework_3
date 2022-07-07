52d0f3cff89afb4f96025fc4f304127c5c4dbe13
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-07-07 15:47:25 +0300

    Внесены изменения в Task_19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,21 +1,25 @@
 ﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
 
-bool Palindrome (int[] array)
+Console.Write ("Enter a Number To Check Palindrome : ");
+
+int number = int.Parse(Console.ReadLine());
+int remineder, sum = 0;
+int temp = number;
+
+while (number > 0)
 {
-    int N = array.Length;
-    bool result = false;
-    for (int i = 0; i < N / 2; i ++)
-    {
-        if (array[i] != array[N-i-1]) 
-        {
-            Console.WriteLine ("It's not a palindrome");
-            return result;
-        }
-    }
-    result = true;
-    
-    Console.WriteLine ("This is a palindrome");
-    return result;
+    remineder = number % 10;
+    sum = (sum * 10) + remineder;
+    number = number / 10; 
 }
-
-
+            
+    if (temp == sum)
+    {
+        Console.WriteLine ($"Number {temp} is Palindrome.");
+    }
+    else
+    {
+        Console.WriteLine($"Number {temp} is not Palindrome");
+    }
+        Console.ReadKey();
+        
